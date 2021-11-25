acfa64c0ea410bb911e7f1506aec01256146b358
shimeeaktar1999 <70462907+shimeeaktar1999@users.noreply.github.com>
2021-11-26 00:37:33 +0530

    Add files via upload

---
+++ Day2.2.java
@@ -0,0 +1,15 @@
+//27. Remove Element
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0) return 0;
+        int j = 0;
+        for(int i = 0; i<nums.length ; i++){
+            if(nums[i] != val ){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
